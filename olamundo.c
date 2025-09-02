0b74bcfd82c8f4fe9850caa1f826eb61388bf359
Jhonatan B Barbosa <90397151+jhonatangda12@users.noreply.github.com>
2025-09-02 14:17:37 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
- 
-int main() {
-    printf("Olá, Mundo!\n");
-    return 0;
+#include <stdio.h>//biblioteca padrão de entrada e saída
+int main() {//função principal
+    printf("Hello, World!\n");//imprime a mensagem na tela
+    return 0;//indica que o programa terminou com sucesso
 }
